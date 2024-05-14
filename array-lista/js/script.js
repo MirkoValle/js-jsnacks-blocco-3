66c79baf1a35a07ebb45ce6dde393c8f29f1f727
MirkoValle <mirko.valle20@gmail.com>
2024-05-14 16:14:43 +0200

    Added ciclo for and console.log for lista

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -3,3 +3,16 @@ const frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino
 frigorifero.push("pesca");
 
 const verificaFrutta = "cocomero";
+let trovato = ("Oh no, devo uscire a comprare il cocomero!")
+
+for (let i = 0; i < frigorifero.length; i++) {
+
+
+    if (frigorifero[i] == verificaFrutta) {
+
+        trovato = ("Trovato! Devo solo preparare il cocktail.")
+
+    }
+}
+
+console.log(trovato)
