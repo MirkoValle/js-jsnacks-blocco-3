ff2e04a99963f2b2cb814aa030bbc9954812a371
MirkoValle <mirko.valle20@gmail.com>
2024-05-14 15:53:28 +0200

    Added array "frigorifero" and costanti

---
+++ array-lista/js/script.js
@@ -0,0 +1,5 @@
+const frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+
+frigorifero.push("pesca");
+
+const verificaFrutta = "cocomero";
