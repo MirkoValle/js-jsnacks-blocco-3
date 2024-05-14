ca43c22ae0750cb096a1cf04818e20f99ad8d6eb
MirkoValle <mirko.valle20@gmail.com>
2024-05-14 16:49:19 +0200

    Added cycle for printing

--- array-tail/js/script.js
+++ array-tail/js/script.js
@@ -1,6 +1,5 @@
 const array = []
-const userNumber = Number.parseInt(prompt("Inserisci un numero"), 10);
-
+let userNumber = Number.parseInt(prompt("Inserisci la quantià di numeri da inserire nell'array"), 10);
 if (userNumber < 1 || userNumber > 100 || isNaN(userNumber)) {
     userNumber = Math.floor(Math.random() * 100 + 1);
 }
@@ -10,3 +9,15 @@ for (let i = 0; i < userNumber; i++) {
 
 }
 
+console.log(array)
+
+const numberPrint = Number.parseInt(prompt("Inserisci la quantità di numeri da stampare"), 10);
+if (userNumber < 1 || userNumber > array.length || isNaN(userNumber)) {
+    userNumber = Math.floor(Math.random() * array.length + 1);
+}
+
+
+
+for (let k = numberPrint; k > 0; k--) {
+    console.log(array[array.length - k])
+}
