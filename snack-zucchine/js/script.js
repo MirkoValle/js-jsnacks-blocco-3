d317571ad344b2215c1b39091d434f64c1179719
MirkoValle <mirko.valle20@gmail.com>
2024-05-24 11:29:42 +0200

    Added length differance

--- snack-zucchine/js/script.js
+++ snack-zucchine/js/script.js
@@ -4,23 +4,38 @@
 // Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
 const ceste = [
-    { varieta: 'Nera di Milano', peso: 341.23, lunghezza: 14.58 },
-    { varieta: 'Romanesca', peso: 287.45, lunghezza: 19.75 },
-    { varieta: 'Fiorentina', peso: 458.12, lunghezza: 22.36 },
-    { varieta: 'Tonda', peso: 132.67, lunghezza: 25.98 },
-    { varieta: 'Lunga fiorentina', peso: 376.54, lunghezza: 28.45 },
-    { varieta: 'Striata d\'Italia', peso: 231.78, lunghezza: 11.23 },
-    { varieta: 'Bianca triestina', peso: 314.89, lunghezza: 17.56 },
-    { varieta: 'Gialla', peso: 459.31, lunghezza: 12.67 },
-    { varieta: 'Rugosa friulana', peso: 278.44, lunghezza: 26.89 },
-    { varieta: 'Patisson', peso: 387.56, lunghezza: 29.12 }
+    { varieta: 'Nera di Milano', peso: 341.23, lunghezza: 4.58 },
+    { varieta: 'Romanesca', peso: 287.45, lunghezza: 9.75 },
+    { varieta: 'Fiorentina', peso: 458.12, lunghezza: 12.36 },
+    { varieta: 'Tonda', peso: 132.67, lunghezza: 15.98 },
+    { varieta: 'Lunga fiorentina', peso: 376.54, lunghezza: 18.45 },
+    { varieta: 'Striata d\'Italia', peso: 231.78, lunghezza: 1.23 },
+    { varieta: 'Bianca triestina', peso: 314.89, lunghezza: 7.56 },
+    { varieta: 'Gialla', peso: 459.31, lunghezza: 2.67 },
+    { varieta: 'Rugosa friulana', peso: 278.44, lunghezza: 16.89 },
+    { varieta: 'Patisson', peso: 387.56, lunghezza: 19.12 }
 ];
 
 let sum = 0;
 
 ceste.forEach(zucchina => {
-    sum += zucchina.peso
+    sum += zucchina.peso;
 });
 
-console.log(sum)
-console.log(parseInt(sum))
+console.log(sum);
+console.log(parseInt(sum));
+
+let zucchinePiccole = [];
+let zucchineGrandi = [];
+
+ceste.forEach(zucchina => {
+    if (zucchina.lunghezza > 8) {
+        zucchineGrandi.push(zucchina)
+    } else {
+        zucchinePiccole.push(zucchina)
+    }
+});
+
+console.log(zucchineGrandi)
+console.log(zucchinePiccole)
+
